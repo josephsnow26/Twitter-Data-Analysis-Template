8a84354d136c154ccfccabd1539bf231c7728ae9
joseph <57895391+josephsnow26@users.noreply.github.com>
2022-08-17 02:40:37 +0100

    Update python.py

--- python.py
+++ python.py
@@ -7,7 +7,7 @@ import plotly.express as px
 st.set_page_config(page_title = 'Dashboard of employees and clients')
 st.title('Dashboard of employees and clients')
 
-YY = px.bar([1,2,2,3,3,4,4,4,4,4,44]
+YY = px.bar([1,2,2,3,3,4,4,4,4,4,44],
 title = '<b>Sex</b>')
 st.plotly_chart(YY)
 
