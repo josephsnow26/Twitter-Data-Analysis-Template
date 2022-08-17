ecddcc2e6044d4c40874b2142f13b0baa0dc4ace
joseph <57895391+josephsnow26@users.noreply.github.com>
2022-08-17 02:37:59 +0100

    Update python.py

--- python.py
+++ python.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.set_page_config(page_title = 'Dashboard of employees and clients',
+st.set_page_config(page_title = 'Dashboard of employees and clients')
 st.title('Dashboard of employees and clients')
 
 
